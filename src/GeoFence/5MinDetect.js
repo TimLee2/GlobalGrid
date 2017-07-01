c299ebedb8c6a611b64c8c27993aa03379bd26cb
timlee2 <tim_lee2@baylor.edu>
2017-07-01 00:07:50 -0500

    Created 5MinDetect.js

---
+++ src/GeoFence/5MinDetect.js
@@ -0,0 +1,40 @@
+/*File: 5MinDtect.js
+* Author: Tim Lee
+* Date Created: 6/30/2017
+
+* This file will determine whether a coordinate needs to change based
+* on the 5-minute mark.
+*/
+
+/*
+* checkIdle
+* latitude: the latitude coordinate
+* longitude: the longitude coordinate
+* idleTime: 300 seconds
+*/
+function checkIdle(latitude, longitude, idleTime, increment){
+    var item = {lat: latitude, lon: longitude, time: Math.round(Date.now()/1000)};
+
+    locationHistory.push(item);
+
+    while(locationHistory.length > 1){
+
+        var temp = locationHistory[0].time;
+        var lastElement = locationHistory.slice(-1)[0].time;
+        var diff = lastElement - temp;
+
+        if(diff > (idleTime+60)){
+            locationHistory.shift();
+        }
+        else if(diff < idleTime){
+            return false;
+        }
+        else if (diff >= idleTime && diff <= (idleTime+60)){
+            var coords1 = GenIDFromCoord(locationHistory[0].lat, locationHistory[0].lon, increment);
+            var coords2 = GenIdFromCoord(locationHistory.slice(-1)[0].lat, locationHistory.slice(-1)[0].lon, increment);
+            if(coords1 == coords2){
+                return true;
+            }
+        }
+    }
+}
